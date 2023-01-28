8b0b41136f3be5faba3711e085bbbdc9c5331432
Victor Valeev <viktorval2001@gmail.com>
2023-01-28 16:33:21 +0300

    remove useless code

--- public/main.js
+++ public/main.js
@@ -5,7 +5,6 @@ const AUTH_API_URL = 'http://127.0.0.1:1337/api/auth';
 const elError = document.getElementById('error');
 const elUser = document.getElementById('user');
 const elBtnMetamask = document.getElementById('auth-metamask');
-const elBtnPhantom = document.getElementById('auth-phantom');
 
 const handleApiPost = async (endpoint, params) => {
   const result = await axios.post(`${AUTH_API_URL}/${endpoint}`, params, {
@@ -63,38 +62,6 @@ const handleAuth = async () => {
   renderUser(user);
 };
 
-const connectToPhantom = async () => {
-  const provider = window.solana;
-  if (provider.isPhantom) {
-    await provider.connect();
-    const solAddress = provider.publicKey.toBase58();
-    return { signer: provider, account: solAddress };
-  }
-  throw new Error('Not connected to Phantom');
-};
-
-const handleAuthSol = async () => {
-  // Connect to Phantom
-  const { signer, account } = await connectToPhantom();
-
-  if (!account) {
-    throw new Error('No account found');
-  }
-
-  const { message } = await requestMessage(account, 'solana');
-
-  const encodedMessage = new TextEncoder().encode(message);
-
-  const signedMessage = await signer.signMessage(encodedMessage);
-
-  // eslint-disable-next-line no-undef
-  const signature = Base58.encode(signedMessage.signature);
-
-  const { user } = await verifyMessage(message, signature, 'solana');
-
-  renderUser(user);
-};
-
 const renderUser = (user) => {
   elUser.innerHTML = user ? JSON.stringify(user, null, 2) : '';
 };
@@ -106,10 +73,6 @@ const renderError = (error) => {
 function init() {
   elBtnMetamask.addEventListener('click', async () => {
     handleAuth().catch((error) => renderError(error));
-  });
-
-  elBtnPhantom.addEventListener('click', async () => {
-    handleAuthSol().catch((error) => renderError(error));
   });
 }
 
