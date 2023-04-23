05796420dd5c7e3741d8b51ff6c468af15811ca4
Victor Valeev <viktorval2001@gmail.com>
2023-04-23 18:43:15 +0300

    some crng30

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,23 @@ app.options("/*", function(req, res, next){
   res.send(200);
 });
 
+module.exports = (req, res) => {
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader('Access-Control-Allow-Origin', 'https://nft-game-2-client.vercel.app');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
+
+//---- other code
+
+//Preflight CORS handler
+  if(req.method === 'OPTIONS') {
+      return res.status(200).json(({
+          body: "OK"
+      }))
+  }
+
+}
+
 // app.options('*', cors())
 // app.use('/*', function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*123");
