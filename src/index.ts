f50f2fa99b7c5d4de503e48b09f2e0681a0ff5a2
Victor Valeev <viktorval2001@gmail.com>
2023-04-23 15:49:01 +0300

    some crng8

--- src/index.ts
+++ src/index.ts
@@ -14,16 +14,15 @@ Moralis.start({
 });
 
 var corsOptions = {
-  origin: config.CLIENT_URL
+  origin: config.CLIENT_URL,
+  preflightContinue: true
 };
+
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
-
-app.use(cors(corsOptions));
-
-app.options('*', cors(corsOptions));
 
 app.use(`/${config.SERVER_ENDPOINT}`, parseServer);
 app.use('/dashboard', parseDashboard);
