d660433152670475798ddb4a21df9bb4c8cc5f5d
Victor Valeev <viktorval2001@gmail.com>
2023-04-23 19:57:14 +0300

    some crng34

--- src/index.ts
+++ src/index.ts
@@ -41,29 +41,7 @@ app.use(cors(corsOptions));
 //   next();
 // });
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
 
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
 
 // module.exports = (req, res) => {
 //   //set header first to allow request or origin domain (value can be different)
