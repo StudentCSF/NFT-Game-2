7f48882979cfd84f42775e209e2ea69910f0755b
Victor Valeev <viktorval2001@gmail.com>
2023-04-23 16:32:46 +0300

    some crng10

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,11 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 app.options('*', cors())
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
