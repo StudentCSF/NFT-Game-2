d3a38d720f25f2838cc84bec3fbb7b1b951ac045
Victor Valeev <viktorval2001@gmail.com>
2023-04-24 21:54:00 +0300

    some crng70

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,6 @@ var corsOptions = {
 
 app.use(cors());
 
-app.options("*", cors())
-
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
