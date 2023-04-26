77c157ef7caa2e93d2c42492a31200acb54dddd8
Victor Valeev <viktorval2001@gmail.com>
2023-04-26 15:59:42 +0300

    some crng90

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ var corsOptions = {
 
 app.use(cors());
 
-app.options("/*", function(req, res, next){
+app.options("/(.*)", function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
