7c548b3b739ce3d7f3f557b09f48ef9e8a0a0e89
Victor Valeev <viktorval2001@gmail.com>
2023-04-26 20:54:47 +0300

    come crng118

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
 //   console.log(`${config.APP_NAME} is running on port ${config.PORT}`);
 // });
 
-app.listen(config.PORT, () => {
+app.listen(2443, () => {
   // eslint-disable-next-line no-console
   console.log(`is running on port`);
 });
