5720fc0df35baa794c2f753f607966ed336137e6
Victor Valeev <viktorval2001@gmail.com>
2023-04-26 19:49:31 +0300

    come crng107

--- src/index.ts
+++ src/index.ts
@@ -33,16 +33,16 @@ app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.json({ limit: '50mb' }));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.json({ limit: '50mb' }));
 
-// app.use(`/${config.SERVER_ENDPOINT}`, parseServer);
-app.use('/dashboard', parseDashboard);
-app.use('/api', apiRouter);
-app.use(errorHandler);
+// // app.use(`/${config.SERVER_ENDPOINT}`, parseServer);
+// app.use('/dashboard', parseDashboard);
+// app.use('/api', apiRouter);
+// app.use(errorHandler);
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // app.listen(config.PORT, () => {
 //   // eslint-disable-next-line no-console
@@ -54,4 +54,4 @@ app.listen(2443, () => {
   console.log(`is running on port`);
 });
 
-module.exports = app;
+// module.exports = app;
