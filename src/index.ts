838773133f995e4aae0e008d513b27976e2dbde1
Victor Valeev <viktorval2001@gmail.com>
2023-04-26 17:14:21 +0300

    some crng97

--- src/index.ts
+++ src/index.ts
@@ -7,46 +7,20 @@ import { errorHandler } from './middlewares/errorHandler';
 import config from './config';
 import { apiRouter } from './apiRouter';
 
-const app = express();
+const app = express()
+const PORT = 4000
 
-Moralis.start({
-  apiKey: config.MORALIS_API_KEY,
-});
-
-var corsOptions = {
-  origin: config.CLIENT_URL,
-  optionsSuccessStatus: 200,
-  methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-  allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-UserSession'
-};
-
-app.options("/(.*)", function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-UserSession');
-  res.send(200);
-});
-
-app.use(cors(corsOptions));
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
 
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
 
-app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-app.use(express.json({ limit: '50mb' }));
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
 
-app.use(`/${config.SERVER_ENDPOINT}`, parseServer);
-app.use('/dashboard', parseDashboard);
-app.use('/api', apiRouter);
-app.use(errorHandler);
-
-app.use(express.static('public'));
-
-app.listen(config.PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`${config.APP_NAME} is running on port ${config.PORT}`);
-});
-
-module.exports = app;
+// Export the Express API
+module.exports = app
