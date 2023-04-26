bb8d0b38b35d5cf269c7e4ce30b2ba055fb1ed46
Victor Valeev <viktorval2001@gmail.com>
2023-04-26 17:11:29 +0300

    some crng96

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,12 @@ var corsOptions = {
   allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-UserSession'
 };
 
-// app.options("/(.*)", function(req, res, next){
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-//   res.send(200);
-// });
+app.options("/(.*)", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-UserSession');
+  res.send(200);
+});
 
 app.use(cors(corsOptions));
 
